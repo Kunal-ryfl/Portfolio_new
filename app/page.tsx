c6d0d8e11732998d0e8ae364e46ef55c3cd78e49
Kunal-ryfl <kunalsharma070806@gmail.com>
2024-07-08 12:25:30 +0530

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,17 @@ export default function Home() {
     >
       <h1 className=' text-4xl font-semibold'>Kunal Bhardwaj</h1>
       <p className=' my-5 text-base'>Hi i&apos;m Kunal. A full stack web developer and coding lover.   </p>
-      <motion.div className=" relative"
+      <motion.div className=" relative  rounded-full overflow-hidden aspect-square h-32 w-fit bg-white "
        initial={{x:-20,opacity:0}} 
        animate={{x:0,opacity:1}} 
        transition={{delay:0.5}}
        >
 
-      <Image src={"https://media.licdn.com/dms/image/C4D03AQEP7hT4dZFbpA/profile-displayphoto-shrink_400_400/0/1662795825093?e=1691020800&v=beta&t=oZQNf5ULdQkWcJ_7VxdrQ8AbUzfHbk6zpecdKjRsgJw"} 
-      height={100} width={100} alt='dp'
-      className=' my-5 rounded-full border-neutral-500  border-2 filter grayscale '
+      <Image src={"https://utfs.io/f/5b7584fd-99f5-4b95-9492-50e6acc04481-c21c76.jpg"} 
+       fill   style={{objectFit:'contain'}}  alt='dp'
+      className='  border-neutral-500  border-2 filter  '
       />
+
       </motion.div>
       <p className=' my-5'>I&apos;m currently pursuing my bachelor&apos;s in computer science engineering from Panjab University. I was always fascinated about tech. My fascination has been my motivation to get into tech industry. </p>
     
--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   images:{
     remotePatterns:[
       {
-        hostname:"media.licdn.com"
+        hostname:"utfs.io"
       }
         
     ]
